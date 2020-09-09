a4d1dd057204c955a9246e3c035364a1fa4647a9
Cosmin <cosmin.barbascu@5psolutions.eu>
2020-09-09 13:26:42 +0300

    #11: Migrations, eloquent models, relations http://pm.5psolutions.net/public/index.php?path_info=projects/training-cosmin-barbascu/tasks/11 Finished

--- database/migrations/2020_09_08_113641_create_order_product_table.php
+++ database/migrations/2020_09_08_113641_create_order_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateOrderProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('order_product', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('contact');
-            $table->string('comments');
-            $table->timestamps();
+            $table->integer('product_id');
+            $table->integer('order_id');
+            $table->float('price');
         });
     }
 
@@ -29,6 +28,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_product');
     }
 }
--- database/migrations/2020_09_08_145826_create_reviews_table.php
+++ database/migrations/2020_09_08_145826_create_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('contact');
-            $table->string('comments');
-            $table->timestamps();
+            $table->string('comment');
+            $table->smallInteger('rating');
+            $table->integer('product_id');
         });
     }
 
@@ -29,6 +28,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('reviews');
     }
 }
