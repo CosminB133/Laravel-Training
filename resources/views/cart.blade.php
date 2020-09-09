98152744e9bb33e010df78b403750dcc2f9717c5
Cosmin <cosmin.barbascu@5psolutions.eu>
2020-09-09 16:50:59 +0300

    #13: Cart http://pm.5psolutions.net/public/index.php?path_info=projects/training-cosmin-barbascu/tasks/13 Adjustments

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,12 +4,13 @@
     @foreach ($products as $product)
         <div class="row" style="margin: 10px">
             <div class="col-md-3">
-                <img src="img/{{ $product->id }}" alt="{{ trans('product image') }}" class="img-fluid"
+                <img src="{{ asset('img/' . $product->id) }}" alt="{{ trans('product image') }}" class="img-fluid"
                      style="max-height: 150px; margin-right: 5px">
             </div>
             <div class="col-md-6">
                 <h4>{{ $product->title }}</h4>
                 <p>{{ $product->description }}</p>
+                <p>{{ $product->price }}</p>
             </div>
             <div class="col-md-3">
                 <form action="/cart" method="post">
@@ -26,15 +27,15 @@
         @csrf
         <div class="form-group">
             <label for="name">{{ trans('Name :') }}</label>
-            <input type="text" class="form-control" name="name" id="name">
+            <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
         </div>
         <div class="form-group">
             <label for="contact">{{ trans('Contact details :') }}</label>
-            <input type="text" class="form-control" name="contact" id="contact">
+            <input type="text" class="form-control" name="contact" id="contact" value="{{ old('contact') }}">
         </div>
         <div class="form-group">
             <label for="comments">{{ trans('Comments :') }}</label>
-            <input type="text" class="form-control" name="comments" id="comments">
+            <input type="text" class="form-control" name="comments" id="comments" value="{{ old('comments') }}">
         </div>
         <input type="submit" class="btn btn-success" value="{{ trans('Submit') }}">
     </form>
